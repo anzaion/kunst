ad6de803a1ef7f009784dfb493c29b16e131cc8f
kinessisk <kinessisk@gmail.com>
2018-03-23 11:30:19 +0100

    roll back changes

--- HelloProcessing/src/binaryfigures/BinaryFigure.java
+++ HelloProcessing/src/binaryfigures/BinaryFigure.java
@@ -35,8 +35,7 @@ public class BinaryFigure extends PApplet{
 	 */
 	public void styleforZero() {
 		
-		fill(0, 0, 0);
-		stroke(255, 0, 0);
+		fill(255, 0, 0);
 		strokeWeight(2);
 		
 	}
@@ -46,9 +45,8 @@ public class BinaryFigure extends PApplet{
 	 */
 	public void styleforOne() {
 		
-		fill(0, 0, 0);
-		stroke(0, 255, 0);
-		strokeWeight(2);
+		fill(0, 255, 0);
+		strokeWeight(4);
 		
 	}
 	
